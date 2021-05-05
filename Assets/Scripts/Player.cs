f890f8f5be5c125b71f64c572e32ec0736480f8c
Mike Murray <mikemurray056@gmail.com>
2021-05-05 16:13:10 -0400

    Setting up sprites and player input
    Not sure if the colour changes will work like I want it yet.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveUp(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void MoveDown(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void MoveLeft(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void MoveRight(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void Fire(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    /***Colour Change*****/
+    public void TurnRed(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void TurnBlue(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void TurnBlack(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void TurnWhite(InputAction.CallbackContext context)
+    {
+
+    }
+}
