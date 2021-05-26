5ecdb9c6be698ad22506a6a0b94d7c3361d4bcfa
Mike Murray <mikemurray056@gmail.com>
2021-05-26 16:35:29 -0400

    Changed super bullet from line renderer to a sprite

--- Assets/Scripts/SuperBullet.cs
+++ Assets/Scripts/SuperBullet.cs
@@ -6,13 +6,20 @@ using UnityEngine;
 //like a laser in the updated game.
 public class SuperBullet : MonoBehaviour
 {
-    public LineRenderer bullet;
-    public Vector3[] bulletPoints;
+    //LineRenderer bullet;
+    //BoxCollider2D hitbox;
+    //public Vector3[] bulletPoints;
 
     // Start is called before the first frame update
     void Start()
     {
         Vector3 screenPos = Camera.main.WorldToViewportPoint(GameManager.instance.transform.position);
+        /*bullet = gameObject.AddComponent<LineRenderer>();
+        hitbox = gameObject.AddComponent<BoxCollider2D>();
+        hitbox.isTrigger = true;
+        hitbox.size = new Vector2(0.1f, screenPos.y * GameManager.instance.ScreenBoundaryY() + 1);
+        hitbox.offset = new Vector2(0, 2.5f);
+        
         bulletPoints = new Vector3[2];
         //bulletPoints[0] = new Vector3(GameManager.instance.playerPos.x, GameManager.instance.playerPos.y +
         //GameManager.instance.player.GetComponent<SpriteRenderer>().bounds.extents.y, -1);           //laser positioned at player's nose
@@ -22,7 +29,10 @@ public class SuperBullet : MonoBehaviour
         bullet.positionCount = bulletPoints.Length;
         bullet.startWidth = 0.05f;
         bullet.endWidth = 0.05f;
-        //bullet.SetPositions(bulletPoints);
+        //bullet.SetPositions(bulletPoints);*/
+
+        //set scale of bullet to reach top of screen
+        transform.localScale = new Vector3(1, screenPos.y * GameManager.instance.ScreenBoundaryY() + 1, 1);
     }
 
     // Update is called once per frame
@@ -34,9 +44,12 @@ public class SuperBullet : MonoBehaviour
          * -Continuously remains on screen until rainbow meter runs out
          * -when gauge runs out, line shrinks and then disappears */
         Vector3 screenPos = Camera.main.WorldToViewportPoint(GameManager.instance.transform.position);
-        bulletPoints[0] = new Vector3(GameManager.instance.playerPos.x, GameManager.instance.playerPos.y +
+        /*bulletPoints[0] = new Vector3(GameManager.instance.playerPos.x, GameManager.instance.playerPos.y +
             GameManager.instance.player.GetComponent<SpriteRenderer>().bounds.extents.y - 0.1f, -1);           //laser positioned at player's nose
         bulletPoints[1] = new Vector3(GameManager.instance.playerPos.x, screenPos.y * GameManager.instance.ScreenBoundaryY() + 1, -1);
-        bullet.SetPositions(bulletPoints);
+        bullet.SetPositions(bulletPoints);*/
+
+        transform.position = new Vector3(GameManager.instance.playerPos.x, GameManager.instance.playerPos.y +
+            GetComponent<SpriteRenderer>().bounds.extents.y, -1);
     }
 }
