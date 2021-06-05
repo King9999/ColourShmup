4b5303876b98d45b9785143edd614da8a64e502b
Mike Murray <mikemurray056@gmail.com>
2021-06-05 17:02:00 -0400

    Cursor now moves to all menu options

--- Assets/Scripts/HUD_Menu.cs
+++ Assets/Scripts/HUD_Menu.cs
@@ -18,12 +18,18 @@ public class HUD_Menu : MonoBehaviour
     public Image cursor;
     public static bool muted;
 
+    Vector3[] menus;                  //contains positions of menu elements.
+    int currentMenu;
+    const int START = 0;
+    const int HELP = 1;
+    const int SOUND = 2;
     private void Awake()
     {
         //DontDestroyOnLoad(this);
     }
     void Start()
     {
+        menus = new Vector3[3];
         muted = false;
         menuAppearanceTimer = 1;
         cursor.enabled = false;
@@ -31,6 +37,13 @@ public class HUD_Menu : MonoBehaviour
         helpMenuText.enabled = false;
         soundText.enabled = false;
         soundToggleText.enabled = false;
+
+        //set up cursor position
+        menus[START] = new Vector3(startGameText.transform.position.x - startGameText.rectTransform.rect.width, startGameText.transform.position.y, 0);
+        menus[HELP] = new Vector3(helpMenuText.transform.position.x - helpMenuText.rectTransform.rect.width, helpMenuText.transform.position.y, 0);
+        menus[SOUND] = new Vector3(soundText.transform.position.x - soundText.rectTransform.rect.width, soundText.transform.position.y, 0);
+        cursor.transform.position = menus[START];
+        currentMenu = START;
 
         //get sound setting
         if (muted)
@@ -49,8 +62,39 @@ public class HUD_Menu : MonoBehaviour
     {
         if (context.phase == InputActionPhase.Performed)
         {
+            if (currentMenu + 1 <= menus.Length - 1)
+                currentMenu++;
+            else
+                currentMenu = 0;
+
+            cursor.transform.position = menus[currentMenu];
             //move cursor to next menu option
             Debug.Log("Pressed Down");
+        }
+    }
+
+    public void OnPressUp(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            if (currentMenu - 1 >= 0)
+                currentMenu--;
+            else
+                currentMenu = menus.Length - 1;
+
+            cursor.transform.position = menus[currentMenu];
+            //move cursor to next menu option
+            Debug.Log("Pressed Up");
+        }
+    }
+
+    public void OnSelectedMenu(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            //check what player selected and move to new scene. If sound was selected, toggle on or off
+            
+            Debug.Log("Selected Menu Option");
         }
     }
 
